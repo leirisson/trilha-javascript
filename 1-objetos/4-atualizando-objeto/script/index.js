45c3471bc96f374de0e1203333f7b3257d2f9be0
leirisson <leirissonsouza99@gmail.com>
2025-04-14 09:59:55 -0400

    atualizando a propriedade de um objeto

---
+++ 1-objetos/4-atualizando-objeto/script/index.js
@@ -0,0 +1,10 @@
+const product = {
+    name: "Teclado",
+    quantity: 1000
+}
+
+
+console.log('quantidade: ',product.quantity)
+
+product.quantity = 20
+console.log('quantidade atual: ', product.quantity)
